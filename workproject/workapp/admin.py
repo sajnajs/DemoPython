980e963a474bf1c04b7da48b6fff1de35e26c33d
sajnajs <147379745+sajnajs@users.noreply.github.com>
2023-11-06 14:44:30 +0530

    Delete workproject directory

--- workproject/workapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import place
-
-from .models import team
-
-admin.site.register(place)
-admin.site.register(team)
--- workproject/workapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WorkappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workapp'
